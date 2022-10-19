7e1dcd7308a5d35ec2c9a5a0735c584723b6db4e
Daffa <daffadev.inc@gmail.com>
2022-10-19 22:07:51 +0700

    update

--- components/MintContainer.tsx
+++ components/MintContainer.tsx
@@ -68,9 +68,9 @@ export default function MintContainer() {
 <>
 <Navigation />
     <div className={styles.collectionContainer}>
-      <h1>BOT card</h1>
+      <h1>BOT NFT Project</h1>
 
-      <p>Claim your BOT Card to start.</p>
+      <p>Claim your BOT Member to enter.</p>
 
       <Card className={`${styles.nftBox} ${styles.spacerBottom}`}>
         <Image src={VipCard} width={300} height={300} alt='avatar' />
